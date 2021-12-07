740bfee625290f0cb471ca480d5917bedc50912f
TDKA <teodor.kobilarov@gmail.com>
2021-12-07 09:05:18 +0100

    f-commit

---
+++ src/components/Recipe.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="recipe" @click="openDescription()">
+    <div class="actions">
+      <h3>{{ recipe.title }}</h3>
+      <div class="icons">
+        <span class="material-icons"> task_alt </span>
+        <span @click="deleteRecipe" class="material-icons"> delete </span>
+        <span class="material-icons"> edit </span>
+      </div>
+    </div>
+    <div class="description" v-if="showDescription">
+      <p>{{ recipe.description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showDescription: false,
+      url: "http://localhost:3000/recipes/" + this.recipe.id,
+    };
+  },
+  props: ["recipe"],
+  methods: {
+    openDescription() {
+      this.showDescription = !this.showDescription;
+    },
+    deleteRecipe() {
+      fetch(this.url, { method: "DELETE" });
+    },
+  },
+};
+</script>
+
+<style>
+h3 {
+  cursor: pointer;
+}
+.recipe {
+  background: whitesmoke;
+  margin: 30px auto;
+  padding: 10px 26px;
+  box-shadow: 0 3px 7px rgba(0, 0, 0, 0.3);
+  border-left: 7px solid rgb(212, 0, 0);
+}
+.actions {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.material-icons {
+  color: #333;
+  font-size: 1.8rem;
+  margin-left: 10px;
+  cursor: pointer;
+}
+.material-icons:hover {
+  border-bottom: 1px solid green;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- if there is smth in the array "recipes" => show it  -->
+    <div v-if="recipes.length">
+      <div v-for="recipe in recipes" :key="recipe.id">
+        <Recipe :recipe="recipe" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Recipe from "../components/Recipe.vue";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: { Recipe },
+  data() {
+    return {
+      recipes: [],
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/recipes")
+      .then((res) => res.json())
+      .then((data) => (this.recipes = data))
+      .catch((err) => console.log(err.message));
+  },
+};
 </script>
